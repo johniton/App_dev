26e76c2fc064f70a726b28e6fb076a18fba7e073
johniton <johnitonmas8@gmail.com>
2025-02-13 21:49:53 +0530

    Dry code

--- duolingo/lib/login.dart
+++ duolingo/lib/login.dart
@@ -46,150 +46,96 @@ class _HomePageState extends State<HomePage> {
                 Expanded(
                   child: Column(
                     children: [
-                      Container(
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Column(
-                            children: [
-                              Container(
-                                width: 250,
-                                height: 250,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: AssetImage(duolingo),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 250,
+                              height: 250,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(duolingo),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'Welcome Back Quitter',
+                              style: TextStyle(
+                                color: T,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Better not quit again',
+                              style: TextStyle(color: T),
+                            ),
+                            SizedBox(height: 10),
+                            TextButton.icon(
+                              onPressed: () {
+                                setState(() {
+                                  if (Screencolor == Colors.black) {
+                                    Screencolor = Colors.white;
+                                    T = Colors.black;
+                                  } else {
+                                    Screencolor = Colors.black;
+                                    T = Colors.white;
+                                  }
+                                });
+                              },
+                              label: Icon(Icons.sunny),
+                            ),
+                            SizedBox(height: 200),
+                            Column(
+                              children: [
+                                TextFieldCreator('Email'),
+                                SizedBox(height: 20),
+                                TextFieldCreator('Password'),
+                                SizedBox(height: 20),
+                                Container(
+                                  width: 600,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    color: const Color.fromARGB(
+                                      255,
+                                      97,
+                                      233,
+                                      44,
+                                    ),
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  child: TextButton.icon(
+                                    onPressed: () {},
+                                    label: Text(
+                                      'Login',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: T,
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Text(
-                                'Welcome Back Quitter',
-                                style: TextStyle(
-                                  color: T,
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold,
+                                SizedBox(height: 10),
+                                TextButton.icon(
+                                  onPressed: () {},
+                                  label: Text(
+                                    'Forgot Password?',
+                                    style: TextStyle(
+                                      color: const Color.fromARGB(
+                                        255,
+                                        97,
+                                        233,
+                                        44,
+                                      ),
+                                      fontSize: 15,
+                                    ),
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                'Better not quit again',
-                                style: TextStyle(color: T),
-                              ),
-                              SizedBox(height: 10),
-                              TextButton.icon(
-                                onPressed: () {
-                                  setState(() {
-                                    if (Screencolor == Colors.black) {
-                                      Screencolor = Colors.white;
-                                      T = Colors.black;
-                                    } else {
-                                      Screencolor = Colors.black;
-                                      T = Colors.white;
-                                    }
-                                  });
-                                },
-                                label: Icon(Icons.sunny),
-                              ),
-                              SizedBox(height: 200),
-                              Container(
-                                child: Column(
-                                  children: [
-                                    Container(
-                                      width: 600,
-                                      height: 50,
-                                      decoration: BoxDecoration(
-                                        color: const Color.fromARGB(
-                                          255,
-                                          243,
-                                          228,
-                                          188,
-                                        ),
-                                        borderRadius: BorderRadius.circular(30),
-                                      ),
-                                      child: TextField(
-                                        decoration: InputDecoration(
-                                          label: Text(
-                                            'Email',
-                                            style: TextStyle(fontSize: 15),
-                                          ),
-                                          border: OutlineInputBorder(
-                                            borderRadius: BorderRadius.circular(
-                                              30,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(height: 20),
-                                    Container(
-                                      width: 600,
-                                      height: 50,
-                                      decoration: BoxDecoration(
-                                        color: const Color.fromARGB(
-                                          255,
-                                          243,
-                                          228,
-                                          188,
-                                        ),
-                                        borderRadius: BorderRadius.circular(30),
-                                      ),
-                                      child: TextField(
-                                        decoration: InputDecoration(
-                                          label: Text(
-                                            'Password',
-                                            style: TextStyle(fontSize: 15),
-                                          ),
-                                          border: OutlineInputBorder(
-                                            borderRadius: BorderRadius.circular(
-                                              30,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(height: 20),
-                                    Container(
-                                      width: 600,
-                                      height: 50,
-                                      decoration: BoxDecoration(
-                                        color: const Color.fromARGB(
-                                          255,
-                                          97,
-                                          233,
-                                          44,
-                                        ),
-                                        borderRadius: BorderRadius.circular(30),
-                                      ),
-                                      child: TextButton.icon(
-                                        onPressed: () {},
-                                        label: Text(
-                                          'Login',
-                                          style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                            color: T,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(height: 10),
-                                    TextButton.icon(
-                                      onPressed: () {},
-                                      label: Text(
-                                        'Forgot Password?',
-                                        style: TextStyle(
-                                          color: const Color.fromARGB(
-                                            255,
-                                            97,
-                                            233,
-                                            44,
-                                          ),
-                                          fontSize: 15,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
                     ],
@@ -199,6 +145,23 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Container TextFieldCreator(String text) {
+    return Container(
+      width: 600,
+      height: 50,
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 243, 228, 188),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          label: Text(text, style: TextStyle(fontSize: 15)),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
         ),
       ),
     );
