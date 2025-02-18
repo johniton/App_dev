a2c34fe0e7b68b085a914174d0dc8683f7437e25
johniton <johnitonmas8@gmail.com>
2025-02-18 22:55:24 +0530

    added scrolling to tackle overflow

--- login_ui/lib/main.dart
+++ login_ui/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'LoginPage.dart';
 import 'SignUpPage.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MyApp());
@@ -34,168 +33,174 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: C,
-
       body: SafeArea(
         child: AnimatedContainer(
           duration: Duration(seconds: 1), // Smooth transition
           color: C,
           child: SizedBox(
             width: double.infinity,
-            child: Column(
-              children: [
-                Container(
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Column(
-                      children: [
-                        Container(
-                            width: 250,
-                            height: 250,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: AssetImage(lemonpie)))),
-                        Text(
-                          'Welcome To LemonPie',
-                          style: TextStyle(
-                              color: T,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          'Keep your data Safe',
-                          style: TextStyle(color: T),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextButton.icon(
-                            onPressed: () {
-                              setState(() {
-                                if (C == Colors.black) {
-                                  lemonpie = 'lib/assets/lemonPieNew.png';
-                                  C = Colors.white;
-                                  T = Colors.black;
-                                } else {
-                                  C = Colors.black;
-                                  T = Colors.white;
-                                  lemonpie = 'lib/assets/darkmodelemonPie.png';
-                                }
-                              });
-                            },
-                            label: Icon(Icons.sunny)),
-                        SizedBox(
-                          height: 200,
-                        ),
-                        Container(
-                          child: Column(
-                            children: [
-                              Container(
-                                width: 600,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    color: const Color.fromARGB(
-                                        255, 243, 228, 188),
-                                    borderRadius: BorderRadius.circular(30)),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      label: Text(
-                                        'Email',
-                                        style: TextStyle(fontSize: 15),
-                                      ),
-                                      border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(30))),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Container(
-                                width: 600,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    color: const Color.fromARGB(
-                                        255, 243, 228, 188),
-                                    borderRadius: BorderRadius.circular(30)),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      label: Text(
-                                        'Password',
-                                        style: TextStyle(fontSize: 15),
-                                      ),
-                                      border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(30))),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Container(
-                                width: 600,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    color: Colors.amber[600],
-                                    borderRadius: BorderRadius.circular(30)),
-                                child: TextButton.icon(
-                                    onPressed: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => Login()));
-                                    },
-                                    label: Text(
-                                      'Login',
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold,
-                                          color: T),
-                                    )),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              TextButton.icon(
-                                onPressed: () {},
-                                label: Text(
-                                  'Forgot Password?',
-                                  style: TextStyle(
-                                      color: Colors.amber[600], fontSize: 15),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              SizedBox(
-                                height: 200,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Dont Have an account?',
-                                    style: TextStyle(color: T),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Column(
+                        children: [
+                          Container(
+                              width: 250,
+                              height: 250,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(lemonpie)))),
+                          Text(
+                            'Welcome To LemonPie',
+                            style: TextStyle(
+                                color: T,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Keep your data Safe',
+                            style: TextStyle(color: T),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextButton.icon(
+                              onPressed: () {
+                                setState(() {
+                                  if (C == Colors.black) {
+                                    lemonpie = 'lib/assets/lemonPieNew.png';
+                                    C = Colors.white;
+                                    T = Colors.black;
+                                  } else {
+                                    C = Colors.black;
+                                    T = Colors.white;
+                                    lemonpie =
+                                        'lib/assets/darkmodelemonPie.png';
+                                  }
+                                });
+                              },
+                              label: Icon(Icons.sunny)),
+                          SizedBox(
+                            height: 200,
+                          ),
+                          Container(
+                            child: Column(
+                              children: [
+                                Container(
+                                  width: 600,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                      color: const Color.fromARGB(
+                                          255, 243, 228, 188),
+                                      borderRadius: BorderRadius.circular(30)),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        label: Text(
+                                          'Email',
+                                          style: TextStyle(fontSize: 15),
+                                        ),
+                                        border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(30))),
                                   ),
-                                  TextButton.icon(
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Container(
+                                  width: 600,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                      color: const Color.fromARGB(
+                                          255, 243, 228, 188),
+                                      borderRadius: BorderRadius.circular(30)),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        label: Text(
+                                          'Password',
+                                          style: TextStyle(fontSize: 15),
+                                        ),
+                                        border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(30))),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Container(
+                                  width: 600,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.amber[600],
+                                      borderRadius: BorderRadius.circular(30)),
+                                  child: TextButton.icon(
                                       onPressed: () {
                                         Navigator.push(
                                             context,
                                             MaterialPageRoute(
-                                                builder: (context) =>
-                                                    SignUp()));
+                                                builder: (context) => Login()));
                                       },
-                                      label: Text('SignUp'))
-                                ],
-                              )
-                            ],
+                                      label: Text(
+                                        'Login',
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                            color: T),
+                                      )),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                TextButton.icon(
+                                  onPressed: () {},
+                                  label: Text(
+                                    'Forgot Password?',
+                                    style: TextStyle(
+                                        color: Colors.amber[600], fontSize: 15),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                SizedBox(
+                                  height: 200,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'Dont Have an account?',
+                                      style: TextStyle(color: T),
+                                    ),
+                                    TextButton.icon(
+                                        onPressed: () {
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      SignUp()));
+                                        },
+                                        label: Text(
+                                          'SignUp',
+                                          style: TextStyle(
+                                              color: const Color.fromARGB(
+                                                  255, 93, 0, 255)),
+                                        ))
+                                  ],
+                                )
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
